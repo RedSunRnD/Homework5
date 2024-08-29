548be8de4c3c711caafce87fb399f5d690b162cd
RedSunRnD <vvatutin.rs@gmail.com>
2024-08-29 11:13:19 +0300

    Corrected homework

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //Задача №1
         int clientOS = 1;  //0 — iOS, 1 — Android
-                if (clientOS == 0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке.");
@@ -10,43 +10,24 @@ public class Main {
         System.out.println();
 
         //Задача №2
-        int clientOSB = 0;
+        int clientOSB = 1;
         int clientDeviceYear = 2015;
-        if (clientOSB == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-            }
-        } else {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        if (clientOSB == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOSB == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOSB == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOSB == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println();
 
         //Задача №3
         //Решение №1
         int year = 2024;
-        /*
         if (year >= 1584 && ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))) {
             System.out.println(year + " год является високосным.");
-        } else {
-            System.out.println(year + " год не является високосным.");
-        }
-        */
-
-        //Решение №2
-        if (year >= 1584){
-            if (year % 400 == 0){
-                System.out.println(year + " год является високосным.");
-            } else if (year % 100 != 0 && year % 4 == 0) {
-                System.out.println(year + " год является високосным.");
-            } else {
-                System.out.println(year + " год не является високосным.");
-            }
         } else {
             System.out.println(year + " год не является високосным.");
         }
@@ -54,18 +35,15 @@ public class Main {
 
         //Задача №4
         int deliveryDistance = 29;
-        int deliveryTime = 0;
-        if (deliveryDistance > 100) {
-            System.out.println("Доставки нет.");
-        } else {
-            if (deliveryDistance <= 20) {
-                deliveryTime += 1;
-            } else if (deliveryDistance <= 60) {
-                deliveryTime += 2;
-            } else {
-                deliveryTime += 3;
-            }
-            System.out.println("Потребуется дней: " + deliveryTime);
+
+        if (deliveryDistance <= 20) {
+            System.out.println("На доставку потребуется 1 день");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("На доставку потребуется 2 дня");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("На доставку потребуется 3 дня");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
         }
         System.out.println();
 
